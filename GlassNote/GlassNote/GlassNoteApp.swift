fd759e232ed099573d381a4542788bd02e0ec57c
avdklk <46193979+avdklk@users.noreply.github.com>
2025-11-17 21:58:37 +0900

    init
    init

---
+++ GlassNote/GlassNote/GlassNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassNoteApp.swift
+//  GlassNote
+//
+//  Created by jyh on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GlassNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
